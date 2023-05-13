d77d3e57a5eba386adaec6dd19681ec91b2fc708
Peter Pajor <saxumvermes@protonmail.com>
2023-05-13 02:32:49 +0200

    Add version info.

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import cors from 'cors';
 import {authHandler} from './auth.js';
 import {ProxiesRoutes} from './routes.js';
 import {config} from "./config.js";
+import {toJsonString} from "./common/utils.js";
 
 const app: Express = express();
 
@@ -13,6 +14,11 @@ app.use(cors({
 }));
 
 app.use(json());
+
+app.get('/version', (req: Request, res: Response) => {
+  res.send(toJsonString({version: process.env.VERSION ?? ''}))
+});
+
 app.use(authHandler);
 app.use('/api/v0', ProxiesRoutes)
 
